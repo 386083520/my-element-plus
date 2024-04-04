9aeef93b35bccd3887ddbee0ef8604377bf90a94
gsd <386083520@qq.com>
2024-04-04 11:05:32 +0800

    monorepo管理子项目

--- play/main.ts
+++ play/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './app.vue'
-import MyButton from '../packages/components/button'
+import MyButton from '@my-element-plus/components/button'
 import { Plugin } from 'vue'
 const app = createApp(App)
 app.use(MyButton as any as Plugin)

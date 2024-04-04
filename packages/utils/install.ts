bc839ea5ba453109a114b9ceed1ef7707a7d88a5
gsd <386083520@qq.com>
2024-04-04 17:17:33 +0800

    ts类型报错修复

--- packages/utils/install.ts
+++ packages/utils/install.ts
@@ -1,7 +1,7 @@
-import { Plugin } from 'vue'
-export function withInstall(comp) {
-    (comp as Plugin).install = function(app) {
-        app.component(comp.name, comp)
+import type { Plugin, App } from 'vue'
+export function withInstall<T>(comp:T) {
+    (comp as Plugin).install = function(app:App) {
+        app.component((comp as any).name, comp as any)
     }
     return (comp as Plugin)
 }

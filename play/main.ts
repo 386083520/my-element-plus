ff17219e635c53eae3cad46fe9d55ad384810537
gsd <386083520@qq.com>
2024-04-07 19:09:24 +0800

    引入并使用原生elementplus

--- play/main.ts
+++ play/main.ts
@@ -3,7 +3,11 @@ import App from './app.vue'
 import MyButton from '@my-element-plus/components/button'
 import EllCard from '@my-element-plus/components/card'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 const app = createApp(App)
 app.use(MyButton)
 app.use(EllCard)
+app.use(ElementPlus)
 app.mount("#app")

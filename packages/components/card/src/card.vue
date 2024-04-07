89acb527efb1888e5ba71e5da41fe8c54fe3b13b
gsd <386083520@qq.com>
2024-04-07 13:49:21 +0800

    添加card组件基础样式

--- packages/components/card/src/card.vue
+++ packages/components/card/src/card.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="el-card">
+    <div class="el-card is-always-shadow">
         <div class="el-card__header">
             header
         </div>
@@ -16,3 +16,24 @@
         name: 'EllCard'
     })
 </script>
+<style>
+    .el-card {
+        border: 1px solid #e4e7ed;
+        color: #303133;
+        border-radius: 4px;
+    }
+    .el-card__header {
+        padding: 18px 20px;
+        border-bottom: 1px solid #e4e7ed;
+    }
+    .el-card__body {
+        padding: 20px;
+    }
+    .el-card__footer {
+        padding: 18px 20px;
+        border-top: 1px solid #e4e7ed;
+    }
+    .el-card.is-always-shadow {
+        box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.12);
+    }
+</style>

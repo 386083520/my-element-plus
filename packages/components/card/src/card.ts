70e4d30ed93bbd59dd9c7ef1cdce8098a39c4834
gsd <386083520@qq.com>
2024-04-08 15:44:16 +0800

    通过 bodyClass控制样式

---
+++ packages/components/card/src/card.ts
@@ -0,0 +1,18 @@
+export const cardProps = {
+    header: {
+        type: String,
+        default: ''
+    },
+    shadow: {
+        type: String,
+        default: 'always'
+    },
+    bodyStyle:{
+        type: Object,
+        default: ''
+    },
+    bodyClass: {
+        type: String,
+        default: ''
+    }
+}

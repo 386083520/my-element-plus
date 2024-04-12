ac5b19f1b8772c4aa01dbb7ebf700d48cec2c62c
gsd <386083520@qq.com>
2024-04-12 20:10:30 +0800

    小写转换为大写

--- scripts/gc.sh
+++ scripts/gc.sh
@@ -7,17 +7,26 @@ fi
 
 FILE_PATH=$(cd "$(dirname ${BASH_SOURCE[0]})/../packages" && pwd)
 DIRNAME="$FILE_PATH/components/$NAME"
+INPUT_NAME=$NAME
 
 if [ -d "$DIRNAME" ]; then
     echo "$NAME 组件已经存在，请检查"
     exit 1
 fi    
 
+NORMALIZED_NAME=""
+for i in $(echo $NAME | sed 's/[_|-]\([a-z]\)/\ \1/;s/^\([a-z]\)/\ \1/'); do
+  C=$(echo "${i:0:1}" | tr "[:lower:]" "[:upper:]")
+  NORMALIZED_NAME="$NORMALIZED_NAME${C}${i:1}"
+done
+NAME=$NORMALIZED_NAME
+echo "$NAME"
+
 echo "$FILE_PATH"
 mkdir -p "$DIRNAME"
 mkdir -p "$DIRNAME/src"
 
-cat > $DIRNAME/src/$NAME.vue <<EOF
+cat > $DIRNAME/src/$INPUT_NAME.vue <<EOF
 <template>
     <div>
         <slot/>
@@ -25,21 +34,21 @@ cat > $DIRNAME/src/$NAME.vue <<EOF
 </template>
 
 <script lang="ts" setup>
-import { ${NAME}Props } from './${NAME}'
+import { ${INPUT_NAME}Props } from './${INPUT_NAME}'
 defineOptions({
     name: 'Ell${NAME}'
 })    
 </script>
 EOF
 
-cat > $DIRNAME/src/$NAME.ts <<EOF
+cat > $DIRNAME/src/$INPUT_NAME.ts <<EOF
 import { buildProps } from "@my-element-plus/utils/vue/props/runtime"
-export const ${NAME}Props = buildProps({})
+export const ${INPUT_NAME}Props = buildProps({})
 EOF
 
 cat <<EOF >"$DIRNAME/index.ts"
 import { withInstall } from "@my-element-plus/utils/install";
-import $NAME from "./src/$NAME.vue";
+import $NAME from "./src/$INPUT_NAME.vue";
 const Ell$NAME  = withInstall($NAME)
 export default Ell$NAME
 EOF

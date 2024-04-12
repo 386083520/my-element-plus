5af2e8fd5cc9fc9b7a941030d1b8fa25e2b1dccc
gsd <386083520@qq.com>
2024-04-12 19:15:57 +0800

    生成vue和ts文件

--- scripts/gc.sh
+++ scripts/gc.sh
@@ -4,3 +4,23 @@ DIRNAME="$FILE_PATH/components/$NAME"
 echo "$FILE_PATH"
 mkdir -p "$DIRNAME"
 mkdir -p "$DIRNAME/src"
+
+cat > $DIRNAME/src/$NAME.vue <<EOF
+<template>
+    <div>
+        <slot/>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ${NAME}Props } from './${NAME}'
+defineOptions({
+    name: 'Ell${NAME}'
+})    
+</script>
+EOF
+
+cat > $DIRNAME/src/$NAME.ts <<EOF
+import { buildProps } from "@my-element-plus/utils/vue/props/runtime"
+export const ${NAME}Props = buildProps({})
+EOF

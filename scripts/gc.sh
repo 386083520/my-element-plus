d501f196a2ef726e01846b2ce1bf37b28419c0ba
gsd <386083520@qq.com>
2024-04-12 19:32:40 +0800

    检查组件是否已经存在

--- scripts/gc.sh
+++ scripts/gc.sh
@@ -1,6 +1,12 @@
 NAME=$1
 FILE_PATH=$(cd "$(dirname ${BASH_SOURCE[0]})/../packages" && pwd)
 DIRNAME="$FILE_PATH/components/$NAME"
+
+if [ -d "$DIRNAME" ]; then
+    echo "$NAME 组件已经存在，请检查"
+    exit 1
+fi    
+
 echo "$FILE_PATH"
 mkdir -p "$DIRNAME"
 mkdir -p "$DIRNAME/src"

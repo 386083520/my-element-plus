9080569ecf7ee2170f7693625426f35ab1f21d21
gsd <386083520@qq.com>
2024-04-24 09:24:26 +0800

    添加旋转动画

--- play/app.vue
+++ play/app.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="main">
     gdasfdsafa
-    <ell-icon color="#409efc" size="30px" :style="{width:'80px',height: '80px'}">
-    <Edit/>
+    <ell-icon color="#409efc" size="30px" :style="{width:'80px',height: '80px'}" class="is-loading">
+      <Loading />
   </ell-icon>
   </div>
 </template>
 
 <script setup  lang="ts">
-import {Edit} from '@element-plus/icons-vue'
+import {Loading} from '@element-plus/icons-vue'
 </script>
 
 <style>

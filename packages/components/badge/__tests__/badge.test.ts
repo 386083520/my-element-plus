753062c48aa77da90c8f225d04b16dec34965a20
gsd <386083520@qq.com>
2024-04-28 12:25:49 +0800

    使测试用例支持使用组件

---
+++ packages/components/badge/__tests__/badge.test.ts
@@ -0,0 +1,8 @@
+import { describe, test,expect } from "vitest"
+import { mount } from '@vue/test-utils'
+import Badge from "../src/badge.vue"
+describe('Badge', () => {
+    test('has value', () => {
+        mount(Badge)
+    })
+})

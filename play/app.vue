1252a99d9a161a182df285cd6866f8c0ff6db9aa
gsd <386083520@qq.com>
2024-05-15 21:17:20 +0800

    其他样式下的朴素按钮样式改造

--- play/app.vue
+++ play/app.vue
@@ -1,17 +1,27 @@
 <template>
-  <!-- <el-row :gutter="20">
-    <el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
-    <el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
-    <el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
-    <el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
-  </el-row> -->
-  <ell-button>default</ell-button>
-  <ell-button plain>default</ell-button>
-  <ell-button type="primary" plain>Primary</ell-button>
+  <!-- <ell-row :gutter="20">
+    <ell-col :span="6"><div class="grid-content ep-bg-purple" /></ell-col>
+    <ell-col :span="6"><div class="grid-content ep-bg-purple" /></ell-col>
+    <ell-col :span="6"><div class="grid-content ep-bg-purple" /></ell-col>
+    <ell-col :span="6"><div class="grid-content ep-bg-purple" /></ell-col>
+  </ell-row> -->
+  <div class="mb-4">
+    <ell-button>Default</ell-button>
+    <ell-button type="primary">Primary</ell-button>
     <ell-button type="success">Success</ell-button>
     <ell-button type="info">Info</ell-button>
     <ell-button type="warning">Warning</ell-button>
     <ell-button type="danger">Danger</ell-button>
+  </div>
+
+  <div class="mb-4">
+    <ell-button plain>Plain</ell-button>
+    <ell-button type="primary" plain>Primary</ell-button>
+    <ell-button type="success" plain>Success</ell-button>
+    <ell-button type="info" plain>Info</ell-button>
+    <ell-button type="warning" plain>Warning</ell-button>
+    <ell-button type="danger" plain>Danger</ell-button>
+  </div>
 </template>
 <script setup lang="ts">
 </script>
@@ -19,13 +29,13 @@
 .ep-bg-purple {
   background-color: aquamarine;
 }
-.el-row {
+.ell-row {
   margin-bottom: 20px;
 }
-.el-row:last-child {
+.ell-row:last-child {
   margin-bottom: 0;
 }
-.el-col {
+.ell-col {
   border-radius: 4px;
 }
 

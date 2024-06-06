ed97bc52efba60adc5016437f1dd3e2997e0da0f
gsd <386083520@qq.com>
2024-06-06 08:52:12 +0800

    自定义颜色的朴素模式

--- packages/components/button/src/button-custom.ts
+++ packages/components/button/src/button-custom.ts
@@ -11,17 +11,31 @@ export function useButtonCustomStyle(props) {
             const color = new TinyColor(buttonColor)
             const activeBgColor = color.mix('#141414', 20).toString()
             const hoverBgColor = color.tint(30).toString()
-            styles = ns.cssVarBlock({
-                'bg-color': buttonColor,
-                'text-color': '#fff',
-                'border-color': buttonColor,
-                'hover-bg-color': hoverBgColor,
-                'hover-text-color': '#fff',
-                'hover-border-color': hoverBgColor,
-                'active-bg-color': activeBgColor,
-                'active-text-color': '#fff',
-                'active-border-color': activeBgColor
-            })
+            if(props.plain) {
+                styles = ns.cssVarBlock({
+                    'bg-color': color.tint(90).toString(),
+                    'text-color': buttonColor,
+                    'border-color': color.tint(50).toString(),
+                    'hover-bg-color': buttonColor,
+                    'hover-text-color': '#fff',
+                    'hover-border-color': buttonColor,
+                    'active-bg-color': activeBgColor,
+                    'active-text-color': '#fff',
+                    'active-border-color': activeBgColor
+                })
+            }else {
+                styles = ns.cssVarBlock({
+                    'bg-color': buttonColor,
+                    'text-color': '#fff',
+                    'border-color': buttonColor,
+                    'hover-bg-color': hoverBgColor,
+                    'hover-text-color': '#fff',
+                    'hover-border-color': hoverBgColor,
+                    'active-bg-color': activeBgColor,
+                    'active-text-color': '#fff',
+                    'active-border-color': activeBgColor
+                })
+            }
         }
         return styles
     })

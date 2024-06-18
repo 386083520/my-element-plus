9633f775a0c74fb1e94445216ca280fb80e70e42
gsd <386083520@qq.com>
2024-06-18 11:25:59 +0800

    button测试用例（3）

--- packages/components/button/__tests__/button.test.tsx
+++ packages/components/button/__tests__/button.test.tsx
@@ -19,4 +19,24 @@ describe('Button', () => {
         expect(wrapper.classes()).toContain('is-loading')
         expect(wrapper.findComponent(Loading).exists()).toBeTruthy()
     })
+    test('size', () => {
+        const wrapper = mount(() => <Button size="large"/>)
+        expect(wrapper.classes()).toContain('ell-button--large')
+    })
+    test('plain', () => {
+        const wrapper = mount(() => <Button plain/>)
+        expect(wrapper.classes()).toContain('is-plain')
+    })
+    test('round', () => {
+        const wrapper = mount(() => <Button round/>)
+        expect(wrapper.classes()).toContain('is-round')
+    })
+    test('circle', () => {
+        const wrapper = mount(() => <Button circle/>)
+        expect(wrapper.classes()).toContain('is-circle')
+    })
+    test('link', () => {
+        const wrapper = mount(() => <Button link/>)
+        expect(wrapper.classes()).toContain('is-link')
+    })
 })

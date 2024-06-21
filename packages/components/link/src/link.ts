df0cfdf0fe507e7020d34a8cc1cc5ff57983b3f4
gsd <386083520@qq.com>
2024-06-21 22:37:52 +0800

    type控制不同的颜色

--- packages/components/link/src/link.ts
+++ packages/components/link/src/link.ts
@@ -1,2 +1,8 @@
 import { buildProps } from "@my-element-plus/utils"
-export const linkProps = buildProps({})
+export const linkProps = buildProps({
+    type: {
+        type: String,
+        values: ['primary', 'success', 'warning', 'info', 'danger', 'default'],
+        default: 'default'
+    }
+})
--- packages/components/link/src/link.vue
+++ packages/components/link/src/link.vue
@@ -13,8 +13,10 @@ import { useNamespace } from '@my-element-plus/hooks';
 defineOptions({
     name: 'EllLink'
 })
+const props = defineProps(linkProps)
 const ns = useNamespace('link')
 const linkCls = computed(() => [
-  ns.b()
+  ns.b(),
+  ns.m(props.type)
 ])
 </script>

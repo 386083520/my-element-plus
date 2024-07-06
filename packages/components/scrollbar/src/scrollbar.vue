c8748ca0984837e935f3cfc0ad6d7d1f69b7bff6
gsd <386083520@qq.com>
2024-07-06 16:14:57 +0800

    完善scrollbar基础结构

--- packages/components/scrollbar/src/scrollbar.vue
+++ packages/components/scrollbar/src/scrollbar.vue
@@ -1,14 +1,29 @@
 <template>
-    <div>
-        <div>
-            <slot/>
+    <div :class="ns.b()">
+        <div :class="wrapKls">
+            <div :class="resizeKls">
+                <slot/>
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { useNamespace } from '@my-element-plus/hooks';
 import { scrollbarProps } from './scrollbar'
+import { computed } from 'vue';
 defineOptions({
     name: 'EllScrollbar'
+})
+const ns = useNamespace('scrollbar')
+const wrapKls = computed(() => {
+    return [
+        ns.e('wrap')
+    ]
+})
+const resizeKls = computed(() => {
+    return [
+        ns.e('view')
+    ]
 })
 </script>

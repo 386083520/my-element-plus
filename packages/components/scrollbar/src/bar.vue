c5df1dc1c3ba334ffc5a5f7cd2a2e52fcdd6d62f
gsd <386083520@qq.com>
2024-07-08 11:33:32 +0800

    调整高度计算细节

--- packages/components/scrollbar/src/bar.vue
+++ packages/components/scrollbar/src/bar.vue
@@ -5,13 +5,15 @@
 import Thumb from './thumb.vue'
 import { scrollbarContextKey } from './constants';
 import { inject, ref } from 'vue';
+import { GAP } from "./util";
+
 const sizeHeight = ref('')
 
 const scrollbar = inject(scrollbarContextKey)
 const update = () => {
     const wrap = scrollbar?.wrapElement
     if(!wrap) return
-    const offsetHeight = wrap.offsetHeight
+    const offsetHeight = wrap.offsetHeight - GAP
     const originalHeight = offsetHeight ** 2 / wrap.scrollHeight
     sizeHeight.value =  `${originalHeight}px`
 }
--- packages/components/scrollbar/src/util.ts
+++ packages/components/scrollbar/src/util.ts
@@ -1,3 +1,5 @@
+export const GAP = 4
+
 export const renderThumbStyle = ({size}) => ({
     'height': size
 })

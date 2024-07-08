d86c29eefffee8b9fa52f040ae5e8d90a366096f
gsd <386083520@qq.com>
2024-07-08 16:30:08 +0800

    点击bar事件

--- packages/components/scrollbar/src/thumb.vue
+++ packages/components/scrollbar/src/thumb.vue
@@ -1,19 +1,24 @@
 <template>
-    <div :class="[ns.e('bar')]">
+    <div :class="[ns.e('bar')]" @mousedown="clickTrackHandler">
         <div :class="ns.e('thumb')" :style="thumbStyle"></div>
     </div>
 </template>
 <script lang="ts" setup>
 import { useNamespace } from '@my-element-plus/hooks'
 import { thumbProps } from './thumb';
-import { computed } from 'vue';
+import { computed, inject } from 'vue';
 import { renderThumbStyle } from './util';
+import { scrollbarContextKey } from './constants';
 
 const props = defineProps(thumbProps)
 const ns = useNamespace('scrollbar')
+const scrollbar = inject(scrollbarContextKey)
 const thumbStyle = computed(() => renderThumbStyle({
     size: props.size,
     move: props.move
 }))
+const clickTrackHandler = (e:MouseEvent) => {
+    scrollbar.wrapElement.scrollTop = 500
+}
 
 </script>

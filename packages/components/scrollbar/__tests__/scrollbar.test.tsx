9c945657aa995786015d4ac2db9ef01583ae18ee
gsd <386083520@qq.com>
2024-07-12 16:47:37 +0800

    scrollbar测试用例（4）

--- packages/components/scrollbar/__tests__/scrollbar.test.tsx
+++ packages/components/scrollbar/__tests__/scrollbar.test.tsx
@@ -82,4 +82,95 @@ describe('ScrollBar', () => {
         offsetHeightRestore()
         scrollHeightRestore()
     })
+
+
+    test('should render height props', () => {
+        const outerHeight = 204
+        const innerHeight = 500
+        const wrapper = mount(() => (
+            <Scrollbar height={`${outerHeight}px`}>
+                <div style={`height: ${innerHeight}px;`}></div>
+            </Scrollbar>
+        ))
+        expect(wrapper.find('.ell-scrollbar__wrap').attributes('style')).toContain('height: 204px;')
+    })
+
+    test('should render max-height props', () => {
+        const outerHeight = 204
+        const innerHeight = 500
+        const wrapper = mount(() => (
+            <Scrollbar max-height={`${outerHeight}px`}>
+                <div style={`height: ${innerHeight}px;`}></div>
+            </Scrollbar>
+        ))
+        expect(wrapper.find('.ell-scrollbar__wrap').attributes('style')).toContain('max-height: 204px;')
+    })
+
+    test('should render always props', () => {
+        const outerHeight = 204
+        const innerHeight = 500
+        const wrapper = mount(() => (
+            <Scrollbar height={`${outerHeight}px`} always>
+                <div style={`height: ${innerHeight}px;`}></div>
+            </Scrollbar>
+        ))
+        expect(wrapper.find('.ell-scrollbar__bar').attributes('style')).toBeFalsy()
+    })
+
+    test('should render min-size props', async () => {
+        const outerHeight = 204
+        const innerHeight = 10000
+        const wrapper = mount(() => (
+            <Scrollbar height={`${outerHeight}px`} always>
+                <div style={`height: ${innerHeight}px;`}></div>
+            </Scrollbar>
+        ))
+        const scrollDom = wrapper.find('.ell-scrollbar__wrap').element
+        const offsetHeightRestore = defineGetter(scrollDom, 'offsetHeight', outerHeight)
+        const scrollHeightRestore = defineGetter(scrollDom, 'scrollHeight', innerHeight)
+        await makeScroll(scrollDom, 'scrollTop', 0)
+        expect(wrapper.find('.is-vertical div').attributes('style')).toContain(
+            'transform: translateY(0%); height: 20px;'
+        )
+        offsetHeightRestore()
+        scrollHeightRestore()
+    })
+
+    test('should render tag props', () => {
+        const wrapper = mount(() => (
+            <Scrollbar tag="ul">
+                {
+                    [1,2,3].map(item => (
+                        <li>{item}</li>
+                    ))
+                }
+            </Scrollbar>
+        ))
+        expect(wrapper.find('.ell-scrollbar__view').element instanceof HTMLUListElement).toBeTruthy()
+    })
+
+    test('should render wrap-style props', () => {
+        const wrapStyle = 'background: red;'
+        const wrapper = mount(() => <Scrollbar wrap-style={wrapStyle}/>)
+        expect(wrapper.find('.ell-scrollbar__wrap').attributes('style')).toContain(wrapStyle)
+    })
+
+    test('should render wrap-class props', () => {
+        const wrapClass = 'test-wrap-class'
+        const wrapper = mount(() => <Scrollbar wrap-class={wrapClass}/>)
+        expect(wrapper.find('.ell-scrollbar__wrap').classes()).toContain(wrapClass)
+    })
+
+
+    test('should render view-style props', () => {
+        const viewStyle = 'background: red;'
+        const wrapper = mount(() => <Scrollbar view-style={viewStyle}/>)
+        expect(wrapper.find('.ell-scrollbar__view').attributes('style')).toContain(viewStyle)
+    })
+
+    test('should render view-class props', () => {
+        const viewClass = 'test-view-class'
+        const wrapper = mount(() => <Scrollbar view-class={viewClass}/>)
+        expect(wrapper.find('.ell-scrollbar__view').classes()).toContain(viewClass)
+    })
 })

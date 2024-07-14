8f4957513a0cc64b24d832f612499c640bc830e0
gsd <386083520@qq.com>
2024-07-14 19:19:19 +0800

    container测试用例（2）

--- packages/components/container/__tests__/container.test.tsx
+++ packages/components/container/__tests__/container.test.tsx
@@ -4,6 +4,8 @@ import Container from "../src/container.vue"
 import Header from "../src/header.vue"
 import Main from "../src/main.vue"
 import { getCssVariable } from "@my-element-plus/test-utils"
+import Aside from "../src/aside.vue"
+import Footer from "../src/footer.vue"
 const AXIOM = 'Rem is the  best girl'
 
 describe('Container', () => {
@@ -52,5 +54,39 @@ describe('Header', () => {
         const vm = wrapper.vm
         expect(getCssVariable(vm.$el, '--ell-header-height')).toEqual('100px')
 
+    })
+})
+
+describe('Aside', () => {
+    test('create aside', () => {
+        const wrapper = mount(() => <Aside></Aside>)
+        expect(wrapper.classes()).toContain('ell-aside')
+    })
+
+    test('aside width', () => {
+        const wrapper = mount(() => <Aside width="200px"></Aside>)
+        const vm = wrapper.vm
+        expect(getCssVariable(vm.$el, '--ell-aside-width')).toEqual('200px')
+    })
+})
+
+describe('Footer', () => {
+    test('create footer', () => {
+        const wrapper = mount(() => <Footer></Footer>)
+        expect(wrapper.classes()).toContain('ell-footer')
+    })
+
+    test('footer height', () => {
+        const wrapper = mount(() => <Footer height="100px"></Footer>)
+        const vm = wrapper.vm
+        expect(getCssVariable(vm.$el, '--ell-footer-height')).toEqual('100px')
+
+    })
+})
+
+describe('Main', () => {
+    test('create main', () => {
+        const wrapper = mount(() => <Main></Main>)
+        expect(wrapper.classes()).toContain('ell-main')
     })
 })

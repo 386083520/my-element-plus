e60563a31e91adb4616209fe1d37283f94dd43ba
gsd <386083520@qq.com>
2024-07-15 07:37:07 +0800

    优化样式引用方式

---
+++ packages/components/container/style/css.ts
@@ -0,0 +1,5 @@
+import '@my-element-plus/theme-chalk/src/container.scss'
+import '@my-element-plus/theme-chalk/src/header.scss'
+import '@my-element-plus/theme-chalk/src/footer.scss'
+import '@my-element-plus/theme-chalk/src/main.scss'
+import '@my-element-plus/theme-chalk/src/aside.scss'
--- play/main.ts
+++ play/main.ts
@@ -21,11 +21,7 @@ import '@my-element-plus/theme-chalk/src/text.scss'
 import EllScrollbar from '@my-element-plus/components/scrollbar'
 import '@my-element-plus/theme-chalk/src/scrollbar.scss'
 import EllContainer from '@my-element-plus/components/container'
-import '@my-element-plus/theme-chalk/src/container.scss'
-import '@my-element-plus/theme-chalk/src/header.scss'
-import '@my-element-plus/theme-chalk/src/footer.scss'
-import '@my-element-plus/theme-chalk/src/main.scss'
-import '@my-element-plus/theme-chalk/src/aside.scss'
+import '@my-element-plus/components/container/style/css.ts'
 
 import '@my-element-plus/theme-chalk/src/reset.scss'
 

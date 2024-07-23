1bd095308bbd8f8b032344c21dd335c2075ccb6a
gsd <386083520@qq.com>
2024-07-23 10:57:03 +0800

    input基础样式

--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -1,12 +1,24 @@
 <template>
-    <div>
-        <input/>
+    <div :class="[containerKls]">
+        <div :class="wrapperKls">
+            <input :class="nsInput.e('inner')"/>
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue';
 import { inputProps } from './input'
+import { useNamespace } from '@my-element-plus/hooks';
+const nsInput = useNamespace('input')
 defineOptions({
     name: 'EllInput'
 })
+const containerKls = computed(() => [
+    nsInput.b()
+])
+const wrapperKls = computed(() => [
+    nsInput.e('wrapper'),
+    nsInput.is('focus')
+])
 </script>

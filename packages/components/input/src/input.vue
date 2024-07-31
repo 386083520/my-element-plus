e3cbfe5a9868529260bf57f85fd92cc63304bc29
gsd <386083520@qq.com>
2024-07-31 11:08:20 +0800

    一键清空逻辑

--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -11,7 +11,11 @@
             @focus="handleFocus"/>
             <span :class="nsInput.e('suffix')">
                 <span :class="nsInput.e('suffix-inner')">
-                    <ell-icon :class="[nsInput.e('icon'), nsInput.e('clear')]">
+                    <ell-icon
+                        v-if="showClear"
+                        :class="[nsInput.e('icon'), nsInput.e('clear')]"
+                        @click="clear"
+                        >
                         <circle-close/>
                     </ell-icon>
                 </span>
@@ -26,8 +30,10 @@ import { inputProps } from './input'
 import { useNamespace } from '@my-element-plus/hooks';
 import { useFocusController } from '@my-element-plus/hooks';
 import { CircleClose } from '@element-plus/icons-vue';
+import { isNil } from 'lodash-unified';
+import { UPDATE_MODEL_EVENT } from '@my-element-plus/constants';
 const nsInput = useNamespace('input')
-const emit = defineEmits(['update:modelValue'])
+const emit = defineEmits([UPDATE_MODEL_EVENT])
 defineOptions({
     name: 'EllInput'
 })
@@ -42,8 +48,19 @@ const wrapperKls = computed(() => [
     nsInput.e('wrapper'),
     nsInput.is('focus', isFocused.value)
 ])
+const nativeInputValue = computed(() => 
+    isNil(props.modelValue) ? '' : String(props.modelValue)
+)
+const showClear = computed(() => 
+    props.clearable &&
+    !!nativeInputValue.value
+)
 const handleInput = (event: Event) => {
     let {value} = event.target as HTMLInputElement
-    emit('update:modelValue', value)
+    emit(UPDATE_MODEL_EVENT, value)
+}
+
+const clear = () => {
+    emit(UPDATE_MODEL_EVENT, '')
 }
 </script>

3ab6b4c6194b17102654657422a4d8b923deaa18
gsd <386083520@qq.com>
2024-08-02 15:48:31 +0800

    设置临时textarea样式

--- packages/components/input/src/utils.ts
+++ packages/components/input/src/utils.ts
@@ -1,7 +1,35 @@
 let hiddenTextarea: HTMLTextAreaElement | undefined = undefined
 
+const CONTEXT_STYLE = [
+    'line-height',
+    'padding-top',
+    'padding-bottom',
+    'padding-left',
+    'padding-right',
+    'font-size',
+    'font-weight',
+    'width',
+    'border-width',
+    'box-sizing',
+    'box-shadow'
+]
+
 type TextAreaHeight = {
     height: string
+}
+
+type NodeStyle = {
+    contextStyle: string
+}
+
+function calculateNodeStyling(targetElement: Element):NodeStyle {
+    const style = window.getComputedStyle(targetElement)
+    const contextStyle = CONTEXT_STYLE.map(
+        (name) => `${name}: ${style.getPropertyValue(name)}`
+    ).join(';')
+    return {
+        contextStyle
+    }
 }
 
 export function calcTextareaHeight(targetElement: HTMLTextAreaElement): TextAreaHeight{
@@ -10,6 +38,8 @@ export function calcTextareaHeight(targetElement: HTMLTextAreaElement): TextArea
         document.body.appendChild(hiddenTextarea)
     }
     hiddenTextarea.value = targetElement.value || targetElement.placeholder || ''
+    const {contextStyle}  = calculateNodeStyling(targetElement)
+    hiddenTextarea.setAttribute('style', `${contextStyle}`)
     let height = hiddenTextarea.scrollHeight
     const result = {} as TextAreaHeight
     result.height = `${height}px`

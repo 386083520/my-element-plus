34623d8cdc4c56ee78ca863301da018f24d79667
gsd <386083520@qq.com>
2024-08-03 17:08:42 +0800

    测试用例（2）

--- packages/components/input/__tests__/input.test.tsx
+++ packages/components/input/__tests__/input.test.tsx
@@ -1,21 +1,30 @@
 import { describe, test,expect } from "vitest"
 import { mount } from '@vue/test-utils'
 import Input from "../src/input.vue"
-import { ref } from "vue"
+import { nextTick, ref } from "vue"
 
 const AXIOM = 'Rem is the  best girl'
 
 describe('Input.vue', () => {
-    test('create', () => {
+    test('create', async () => {
         const input = ref('input')
         const wrapper = mount(() => (
             <Input
                 placeholder="请输入内容"
                 modelValue={input.value}
+                minlength={3}
+                maxlength={5}
             />
         ))
         const inputElm = wrapper.find('input')
+        const nativeInput = inputElm.element
         expect(inputElm.exists()).toEqual(true)
-        
+        expect(nativeInput.placeholder).toMatchInlineSnapshot(`"请输入内容"`)
+        expect(nativeInput.value).toMatchInlineSnapshot(`"input"`)
+        expect(nativeInput.minLength).toMatchInlineSnapshot(`3`)
+
+        input.value = 'text'
+        await nextTick()
+        expect(nativeInput.value).toMatchInlineSnapshot(`"text"`)
     })
 })

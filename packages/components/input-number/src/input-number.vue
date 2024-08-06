a7156ba3ea74c1a3a1060135922a73ea6d9311c9
gsd <386083520@qq.com>
2024-08-06 16:51:11 +0800

    increase和decrease

--- packages/components/input-number/src/input-number.vue
+++ packages/components/input-number/src/input-number.vue
@@ -2,18 +2,24 @@
     <div :class="[
         ns.b()
     ]">
-        <span :class="[ns.e('decrease')]">
+        <span
+            :class="[ns.e('decrease')]"
+            @click="decrease"
+        >
             <ell-icon>
                 <Minus/>
             </ell-icon>
         </span>
-        <span :class="[ns.e('increase')]">
+        <span
+            :class="[ns.e('increase')]"
+            @click="increase"
+        >
             <ell-icon>
                 <Plus/>
             </ell-icon>
         </span>
         <ell-input
-            :model-value="modelValue"
+            :model-value="displayValue"
             @input="handleInput"
         />
     </div>
@@ -24,14 +30,37 @@ import { useNamespace } from '@my-element-plus/hooks';
 import { Minus,Plus } from '@element-plus/icons-vue';
 import { inputNumberEmits, inputNumberProps } from './input-number'
 import { UPDATE_MODEL_EVENT } from '@my-element-plus/constants';
+import { computed, reactive } from 'vue';
 const ns = useNamespace('input-number')
 defineOptions({
     name: 'EllInputNumber'
 })
 const props = defineProps(inputNumberProps)
 const emit = defineEmits(inputNumberEmits)
+interface Data {
+    currentValue: number | null | undefined
+}
+const data = reactive<Data>({
+    currentValue: props.modelValue
+})
+const setCurrentValue = (value: number | null | undefined) => {
+    data.currentValue = value
+    emit(UPDATE_MODEL_EVENT, data.currentValue)
+}
 const handleInput = (value:string) => {
     const newVal = value === '' ? null: Number(value)
     emit(UPDATE_MODEL_EVENT, newVal)
 }
+const decrease = () => {
+    const value = Number(props.modelValue) || 0
+    setCurrentValue(value - 1)
+}
+const increase = () => {
+    const value = Number(props.modelValue) || 0
+    setCurrentValue(value + 1)
+}
+
+const displayValue = computed(() => {
+    return data.currentValue
+})
 </script>

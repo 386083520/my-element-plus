ae7c44b85b01f3362bc9ff012e53bd2a2202a4de
gsd <386083520@qq.com>
2024-08-06 13:04:01 +0800

    input-number基础样式

--- packages/components/input-number/src/input-number.vue
+++ packages/components/input-number/src/input-number.vue
@@ -1,13 +1,26 @@
 <template>
-    <div>
-        <span>-</span>
-        <span>+</span>
+    <div :class="[
+        ns.b()
+    ]">
+        <span :class="[ns.e('decrease')]">
+            <ell-icon>
+                <Minus/>
+            </ell-icon>
+        </span>
+        <span :class="[ns.e('increase')]">
+            <ell-icon>
+                <Plus/>
+            </ell-icon>
+        </span>
         <ell-input/>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { useNamespace } from '@my-element-plus/hooks';
+import { Minus,Plus } from '@element-plus/icons-vue';
 import { inputNumberProps } from './input-number'
+const ns = useNamespace('input-number')
 defineOptions({
     name: 'EllInputNumber'
 })

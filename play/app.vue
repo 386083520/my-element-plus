308cdbc4aadafadaab4f37f3315f6cb6485ad21b
gsd <386083520@qq.com>
2024-08-08 16:41:36 +0800

    按钮位置不同尺寸

--- play/app.vue
+++ play/app.vue
@@ -1,10 +1,27 @@
 <template>
 <ell-input-number
     v-model="num"
+    :min="1"
+    :max="10"
+    controls-position="right"
+    size="large"
+    @change="handleChange"
+  />
+  <ell-input-number
+    v-model="num"
     class="mx-4"
     :min="1"
     :max="10"
     controls-position="right"
+    @change="handleChange"
+  />
+  <ell-input-number
+    v-model="num"
+    :min="1"
+    :max="10"
+    size="small"
+    controls-position="right"
+    @change="handleChange"
   />
 </template>
 

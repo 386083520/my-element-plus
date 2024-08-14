a2b97963d500275d6cef58650cc476ae9c60b889
gsd <386083520@qq.com>
2024-08-14 09:29:36 +0800

    input基础用法及v-model原理

--- play/app.vue
+++ play/app.vue
@@ -1,15 +1,22 @@
 <template>
-<ell-radio v-model="radio" value="disabled">Option A</ell-radio>
+<!-- <ell-radio v-model="radio" value="disabled">Option A</ell-radio>
   <ell-radio v-model="radio" value="selected and disabled">
     Option B
   </ell-radio>
-  {{ radio }}
+  {{ radio }} -->
+   <input type="radio" value="1" v-model="radio1"/>
+   <input type="radio" value="2" v-model="radio1"/>
+   {{ radio1 }}
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 
 const radio = ref('selected and disabled')
+const radio1 = ref('1')
+const handleChange = (e) => {
+  radio1.value = e.target.value
+}
 </script>
 
 <style>

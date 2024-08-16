4e1ac4a18295330d54b030c867932ef5748f2be0
gsd <386083520@qq.com>
2024-08-16 12:27:57 +0800

    radio测试用例（4）

--- packages/components/radio/__tests__/radio.test.tsx
+++ packages/components/radio/__tests__/radio.test.tsx
@@ -3,6 +3,8 @@ import { mount } from '@vue/test-utils'
 
 import Radio from "../src/radio.vue"
 import { ref } from "vue"
+import RadioGroup from "../src/radio-group.vue"
+import RadioButton from "../src/radio-button.vue"
 
 const AXIOM = 'rem is the best girl'
 
@@ -37,5 +39,75 @@ describe('Radio.vue', () => {
         const wrapper = mount(() => <Radio label="a" v-model={radio.value} onChange={handleChange}></Radio>)
         await wrapper.trigger('click')
         expect(changeData.value).toEqual('a')
+    })
+})
+
+describe('radio group', () => {
+    test('create', async () => {
+        const radio = ref(3)
+        const wrapper = mount(() => (
+            <RadioGroup v-model={radio.value}>
+                <Radio value={3}></Radio>
+                <Radio value={6}></Radio>
+                <Radio value={9}></Radio>
+            </RadioGroup>
+        ))
+        const [radio1, radio2, radio3] = wrapper.findAll('.ell-radio')
+        expect(radio1.classes()).toContain('is-checked')
+        await radio2.trigger('click')
+        expect(radio2.classes()).toContain('is-checked')
+        expect(radio.value).toEqual(6)
+    })
+
+    test('disabled', async () => {
+        const radio = ref(3)
+        const wrapper = mount(() => (
+            <RadioGroup v-model={radio.value} disabled>
+                <Radio value={3}></Radio>
+                <Radio value={6}></Radio>
+                <Radio value={9}></Radio>
+            </RadioGroup>
+        ))
+        expect(wrapper.find('label.is-disabled').exists()).toBe(true)
+        const [radio1, radio2, radio3] = wrapper.findAll('.ell-radio')
+        expect(radio1.classes()).toContain('is-checked')
+        await radio2.trigger('click')
+        expect(radio1.classes()).toContain('is-checked')
+        expect(radio.value).toEqual(3)
+    })
+
+    test('change event', async () => {
+        const radio = ref(3)
+        const data = ref(0)
+        function onChange(val) {
+            data.value = val
+        }
+        const wrapper = mount(() => (
+            <RadioGroup v-model={radio.value} onChange={onChange}>
+                <Radio value={3}></Radio>
+                <Radio value={6}></Radio>
+                <Radio value={9}></Radio>
+            </RadioGroup>
+        ))
+        const [radio1, radio2, radio3] = wrapper.findAll('.ell-radio')
+        await radio2.trigger('click')
+        expect(data.value).toEqual(6)
+    })
+
+    test('disabled radio-button', async () => {
+        const radio = ref(3)
+        const wrapper = mount(() => (
+            <RadioGroup v-model={radio.value} disabled>
+                <RadioButton value={3}></RadioButton>
+                <RadioButton value={6}></RadioButton>
+                <RadioButton value={9}></RadioButton>
+            </RadioGroup>
+        ))
+        expect(wrapper.find('label.is-disabled').exists()).toBe(true)
+        const [radio1, radio2, radio3] = wrapper.findAll('.ell-radio-button')
+        expect(radio1.classes()).toContain('is-checked')
+        await radio2.trigger('click')
+        expect(radio1.classes()).toContain('is-checked')
+        expect(radio.value).toEqual(3)
     })
 })

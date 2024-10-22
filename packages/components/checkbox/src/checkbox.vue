1f22306875dbe5a33b05a7d33197667eb0e647a3
gsd <386083520@qq.com>
2024-10-22 15:35:56 +0800

    checkbox基础结构

--- packages/components/checkbox/src/checkbox.vue
+++ packages/components/checkbox/src/checkbox.vue
@@ -1,12 +1,20 @@
 <template>
-    <div>
-        <slot/>
-    </div>
+    <label  :class="ns.b()">
+        <span :class="ns.e('input')">
+            <input type="checkbox" :class="ns.e('original')"/>
+            <span :class="ns.e('inner')"></span>
+        </span>
+        <span :class="ns.e('label')">
+            <slot/>
+        </span>
+    </label>
 </template>
 
 <script lang="ts" setup>
 import { checkboxProps } from './checkbox'
+import { useNamespace } from '@my-element-plus/hooks';
 defineOptions({
     name: 'EllCheckbox'
 })
+const ns = useNamespace('checkbox')
 </script>

fa9fddd70eadd9b8b4d1bce3e504440bba47444f
gsd <386083520@qq.com>
2024-10-29 14:46:20 +0800

    checkbox传入数组选中效果

--- packages/components/checkbox/src/composables/use-checkbox-status.ts
+++ packages/components/checkbox/src/composables/use-checkbox-status.ts
@@ -1,14 +1,16 @@
 import { computed } from "vue";
 import { CheckboxProps } from "../checkbox";
 import { CheckboxModel } from "../composables";
-import { isBoolean } from "@my-element-plus/utils";
+import { isArray, isBoolean } from "@my-element-plus/utils";
 
 export const useCheckboxStatus = (props: CheckboxProps, { model }: Pick<CheckboxModel, 'model'>) => {
     const isChecked = computed<boolean>(() => {
         const value = model.value
         if(isBoolean(value)) {
-            return model.value
-        } else if(value !== null && value !== undefined){
+            return value
+        }else if(isArray(value)) {
+            return value.includes(props.value)
+        }else if(value !== null && value !== undefined){
             return value === props.trueValue
         } else {
             return !!value

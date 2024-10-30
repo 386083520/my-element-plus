e425fb53c380151900d20b4f61c4e34d77a4b273
gsd <386083520@qq.com>
2024-10-30 09:23:50 +0800

    半选中效果

--- play/app.vue
+++ play/app.vue
@@ -1,19 +1,16 @@
 <template>
-<ell-checkbox-group v-model="checkList">
-    <ell-checkbox label="Option 1" value="Value 1" />
-    <ell-checkbox label="Option 2 & Value 2"  value="Value 2"/>
-  </ell-checkbox-group>
-{{ checkList }}
+<el-checkbox
+    v-model="checkAll"
+    :indeterminate="isIndeterminate"
+  >
+    Check all
+  </el-checkbox>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-const checkList = ref(['Value 2'])
+const checkAll = ref(false)
+const isIndeterminate = ref(true)
 </script>
 
-<style>
-.mx-4 {
-  margin: 0 10px;
-}
-</style>

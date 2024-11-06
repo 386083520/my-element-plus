3f762b3865bea26a873cced33d5ac96edc7eeeac
gsd <386083520@qq.com>
2024-11-06 14:51:35 +0800

    checkboxbutton基础逻辑

--- packages/components/checkbox/src/checkbox-button.vue
+++ packages/components/checkbox/src/checkbox-button.vue
@@ -1,13 +1,44 @@
 <template>
-    <label>
-        <input type="checkbox"/>
-        <span>
+    <label :class="labelKls">
+        <input
+            v-if="trueValue || falseValue"
+            :true-value="trueValue ?? true"
+            :false-value="falseValue ?? false"
+            v-model="model"
+            type="checkbox"
+            :class="ns.be('button', 'original')"
+            />
+        <input
+            v-else
+            v-model="model"
+            type="checkbox"
+            :class="ns.be('button', 'original')"
+            :value="value"
+        />    
+        <span :class="ns.be('button', 'inner')">
             <slot></slot>
         </span>
     </label>
 </template>
 <script lang="ts" setup>
+import { useNamespace } from '@my-element-plus/hooks';
+import { computed } from 'vue';
+import { checkboxProps } from './checkbox';
+import { useCheckbox } from './composables/use-checkbox';
+
 defineOptions({
     name: 'EllCheckboxButton'
+})
+const ns = useNamespace('checkbox')
+
+const props = defineProps(checkboxProps)
+
+const { model } = 
+   useCheckbox(props)
+
+const labelKls = computed(() => {
+    return [
+        ns.b('button')
+    ]
 })
 </script>

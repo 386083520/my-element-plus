a4783bbdec1f0411506ffd4ebcd1f05614b71c55
gsd <386083520@qq.com>
2024-11-06 10:36:33 +0800

    验证checkboxgroup

--- play/app.vue
+++ play/app.vue
@@ -1,13 +1,22 @@
 <template>
-  <ell-checkbox v-model="checked3" label="Option1" border size="large"/>
-  <ell-checkbox v-model="checked4" label="Option2" border />
-  <ell-checkbox v-model="checked3" label="Option1" border size="small"/>
-
+  <el-checkbox-group v-model="checkboxGroup1" size="large">
+      <el-checkbox label="Option1" value="Value1" border />
+      <el-checkbox label="Option2" value="Value2" border />
+</el-checkbox-group>
+<el-checkbox-group v-model="checkboxGroup1">
+      <el-checkbox label="Option1" value="Value1" />
+      <el-checkbox label="Option2" value="Value2" border />
+</el-checkbox-group>
+<el-checkbox-group v-model="checkboxGroup1" size="small">
+      <el-checkbox label="Option1" value="Value1" border />
+      <el-checkbox label="Option2" value="Value2" border />
+</el-checkbox-group>
+{{ checkboxGroup1 }}
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-const checked3 = ref(false)
+const checkboxGroup1 = ref([])
 const checked4 = ref(false)
 </script>

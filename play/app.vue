0a172765d7331860afb910b8f0e6ab83bfbd2018
gsd <386083520@qq.com>
2024-11-06 15:21:36 +0800

    checkbox基础样式

--- play/app.vue
+++ play/app.vue
@@ -2,7 +2,8 @@
 
 <ell-checkbox-group v-model="checkboxGroup1">
       <ell-checkbox-button label="Option1" value="Value1">aaa</ell-checkbox-button>
-      <ell-checkbox-button label="Option2" value="Value2" border />
+      <ell-checkbox-button label="Option2" value="Value2">bbb</ell-checkbox-button>
+      <ell-checkbox-button label="Option3" value="Value3">ccc</ell-checkbox-button>
 </ell-checkbox-group>
 {{ checkboxGroup1 }}
 </template>

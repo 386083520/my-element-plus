af9d4f88f46e1c0127dfd9d70f995c58b5a4e11a
gsd <386083520@qq.com>
2024-11-07 08:55:54 +0800

    checkbox测试用例（1）

--- packages/components/checkbox/__tests__/checkbox.test.tsx
+++ packages/components/checkbox/__tests__/checkbox.test.tsx
@@ -2,12 +2,23 @@ import { describe, test, expect } from "vitest"
 import { mount } from '@vue/test-utils'
 
 import Checkbox from "../src/checkbox.vue"
+import { ref } from "vue"
 
 const AXIOM = 'rem is the best girl'
 
 describe('Checkbox.vue', () => {
-    test('render test', () => {
-        const wrapper = mount(() => <Checkbox>{AXIOM}</Checkbox>)
-        expect(wrapper.text()).toEqual(AXIOM)
+    test('create', async () => {
+        const checked = ref(false)
+        const wrapper = mount(() => <Checkbox v-model={checked.value} label="a"></Checkbox>)
+        expect(wrapper.classes()).toContain('ell-checkbox')
+        await wrapper.trigger('click')
+        expect(wrapper.classes()).toContain('is-checked')
+        await wrapper.trigger('click')
+        expect(wrapper.classes('is-checked')).toBe(false)
+    })
+
+    test('label set to number 0', () => {
+        const wrapper = mount(() => <Checkbox  label={0}></Checkbox>)
+        expect(wrapper.find('.ell-checkbox__label').text()).toBe('0')
     })
 })

c421685935822831d64fb8fca3bd1188fbb9576f
gsd <386083520@qq.com>
2024-11-07 09:05:18 +0800

    checkbox测试用例（2）

--- packages/components/checkbox/__tests__/checkbox.test.tsx
+++ packages/components/checkbox/__tests__/checkbox.test.tsx
@@ -21,4 +21,24 @@ describe('Checkbox.vue', () => {
         const wrapper = mount(() => <Checkbox  label={0}></Checkbox>)
         expect(wrapper.find('.ell-checkbox__label').text()).toBe('0')
     })
+
+    test('disabled', async () => {
+        const checked = ref(false)
+        const wrapper = mount(() => <Checkbox v-model={checked.value} label="a" disabled></Checkbox>)
+        expect(wrapper.classes()).toContain('is-disabled')
+        expect(checked.value).toBe(false)
+        await wrapper.trigger('click')
+        expect(checked.value).toBe(false)
+    })
+
+    test('change', async () => {
+        const checked = ref(false)
+        const data = ref()
+        const onChange = (val)  => (data.value = val)
+        const wrapper = mount(() => (
+            <Checkbox v-model={checked.value} label="a" onChange={onChange}></Checkbox>
+        ))
+        await wrapper.trigger('click')
+        expect(data.value).toBe(true)
+    })
 })

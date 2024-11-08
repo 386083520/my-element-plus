da4a1a02fa1ef774180623d199147362634da86e
gsd <386083520@qq.com>
2024-11-08 10:35:56 +0800

    switch基础样式

--- packages/components/switch/src/switch.vue
+++ packages/components/switch/src/switch.vue
@@ -1,15 +1,26 @@
 <template>
-    <div>
+    <div  :class="switchKls">
         <input
         type="checkbox"
+        :class="ns.e('input')"
         />
-        <span></span>
+        <span :class="ns.e('core')">
+            <div :class="ns.e('action')">
+
+            </div>
+        </span>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue';
 import { switchProps } from './switch'
+import { useNamespace } from '@my-element-plus/hooks';
 defineOptions({
     name: 'EllSwitch'
 })
+const ns = useNamespace('switch')
+const switchKls = computed(() => [
+    ns.b()
+])
 </script>

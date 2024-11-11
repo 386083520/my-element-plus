73346d91ec6d97889c9dd51a42e8ad73723a9f89
gsd <386083520@qq.com>
2024-11-11 08:48:58 +0800

    自定义颜色

--- play/app.vue
+++ play/app.vue
@@ -2,6 +2,7 @@
 
 <ell-switch
     v-model="value1"
+    style="--ell-switch-on-color: #13ce66; --ell-switch-off-color: #ff4949"
     active-text="Pay by month"
     inactive-text="Pay by year"/>
 {{value1}}

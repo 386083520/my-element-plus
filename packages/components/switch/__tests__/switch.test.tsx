427f3b14466d06d0304aebf8115591f98d613588
gsd <386083520@qq.com>
2024-11-13 14:56:14 +0800

    测试用例（2）

--- packages/components/switch/__tests__/switch.test.tsx
+++ packages/components/switch/__tests__/switch.test.tsx
@@ -2,6 +2,7 @@ import { describe, test, expect } from "vitest"
 import { mount } from '@vue/test-utils'
 
 import Switch from "../src/switch.vue"
+import { wrap } from "lodash-unified"
 
 const AXIOM = 'rem is the best girl'
 
@@ -19,5 +20,26 @@ describe('Switch.vue', () => {
         expect(vm.$el.classList.contains('is-checked')).false
         const coreEl = vm.$el.querySelector('.ell-switch__core')
         expect(coreEl.style.width).toEqual('100px')
+    })
+
+    test('size', () => {
+        const wrapper = mount(() => <Switch size="large"/>)
+        expect(wrapper.find('.ell-switch--large').exists()).toBe(true)
+    })
+
+    test('inline prompt', () => {
+        const props = {
+            activeText: 'on',
+            inactiveText: 'off',
+            width: 100,
+            inlinePrompt: true
+        }
+        const wrapper = mount(() => <Switch {...props}/>)
+        const vm = wrapper.vm
+        expect(vm.$el.classList.contains('is-checked')).false
+        const coreEl = vm.$el.querySelector('.ell-switch__core')
+        expect(coreEl.style.width).toEqual('100px')
+        const label = wrapper.find('.ell-switch__inner span')
+        expect(label.text()).toEqual('off')
     })
 })
